6d23538fead0bf6f325f1881bb06def90ce4e7f3
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-01-30 12:35:48 +0300

    adding the login page layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="mainComp">
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-export default function MyApp() {
+function index() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("username: ", username);
+    console.log("password: ", password);
+  };
+
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
+    <div
+      style={{ height: "100%" }}
+      className="d-flex flex-column justify-content-center align-items-center"
+    >
+      <h1>Gist Leather Craft</h1> <h2> Sipariş Sistemi</h2>
+      <Form className="d-flex flex-column justify-content-center align-items-center">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Kullanıcı Adı</Form.Label>
+          <Form.Control
+            onChange={(e) => setUsername(e.target.value)}
+            type="text"
+            placeholder="Kullanıcı adını girin"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Şifre</Form.Label>
+          <Form.Control
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Şifre"
+          />
+        </Form.Group>
+
+        <Button onClick={handleClick} variant="primary" type="submit">
+          Onayla
+        </Button>
+      </Form>
     </div>
   );
 }
+
+export default index;
