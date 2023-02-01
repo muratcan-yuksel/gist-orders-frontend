9cd27af1a0e8b2a9b95176804a4c70a44b46ed87
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-02-01 10:12:51 +0300

    filter searchs

--- pages/user.js
+++ pages/user.js
@@ -3,11 +3,13 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { DataContext } from "../context/context";
 import { createClient } from "next-sanity";
+import Form from "react-bootstrap/Form";
 
 const user = ({ products }) => {
   const [context, setContext] = useContext(DataContext);
   const userId = context;
   const [myUser, setMyUser] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const router = useRouter();
   //dunno if this works
@@ -50,7 +52,36 @@ const user = ({ products }) => {
     }
   }
 
-  return <div>{returnState()}</div>;
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = products.filter(
+    (product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.code.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      {returnState()}
+      <Form.Control
+        type="text"
+        placeholder="Search products"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <div>
+        <Form.Select aria-label="Default select example">
+          {filteredProducts.map((product) => (
+            <option value={product._id}>
+              Ürün Adı: {product.name} - Ürün Kodu: {product.code}
+            </option>
+          ))}
+        </Form.Select>
+      </div>
+    </div>
+  );
 };
 
 export default user;
